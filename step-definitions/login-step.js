602b9fc2d3acec97f436a3368bcb4b584761be8f
Anastasiia Kotielnikova <akotelnikova1993@gmail.com>
2023-08-07 17:53:20 +0300

    added login-page

--- step-definitions/login-step.js
+++ step-definitions/login-step.js
@@ -1,15 +1,16 @@
 const { Given, When, Then, defineStep} = require('@cucumber/cucumber')
+const {LoginPage} = require('../page-objects/login-page')
+
+const loginPage = new LoginPage()
 
 defineStep("I visit a login page", async function() {
-    await page.goto('https://www.saucedemo.com/')
+    await loginPage.navigateToLoginScreen()
 })
 
 defineStep("I fill the login form with valid credentials", async function() {
-    await page.fill('#user-name', 'standard_user')
-    await page.fill('#password', 'secret_sauce')
-    await page.click('#login-button')
+    await loginPage.submitLoginForm()
 })
 
 defineStep("I should see the home page", async function() {
-    await page.waitForSelector('.inventory_list')
+    await loginPage.assertUserIsLoggedIn()
 })
